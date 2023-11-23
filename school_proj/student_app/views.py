c19cf8fcbd582f5bd22c67785308dcb10b21b8de
fravila08 <fr4v1l4@gmail.com>
2023-11-23 01:48:55 -0800

    solution complete

--- school_proj/student_app/views.py
+++ school_proj/student_app/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Student
-from .serializers import StudentAllSerializer
+from .serializers import StudentAllSerializer, StudentSerializer
 # Create your views here.
 
 class All_students(APIView):
@@ -10,3 +10,8 @@ class All_students(APIView):
         students = Student.objects.all()
         serialized_students = StudentAllSerializer(students, many=True)
         return Response(serialized_students.data)
+    
+class A_student(APIView):
+    def get(self, request, id):
+        student = get_object_or_404(Student, id = id)
+        return Response(StudentAllSerializer(student).data)
